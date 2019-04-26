5be23e6328ed054645ce04c1a26ed032ee0a8905
jsonpeng <1127915086@qq.com>
2019-04-26 19:39:08 +0800

    ss

--- app/helpers.php
+++ app/helpers.php
@@ -590,11 +590,11 @@ function dealFloatData($data,$limit = 2)
     {
       foreach ($data as $key => $val) 
       {
-        $data[$key] = (float)round($val,$limit);
+        $data[$key] = sprintf("%.2f", $val);
       }
     }
     else{
-      $data = round($data,$limit);
+      $data = sprintf("%.2f", $data);
     }
     return is_array($data) ? $data : (float)$data;
 }
