c00e3fe00fd4cb6bda86d3d0f2d733eb89cdd99b
jsonpeng <1127915086@qq.com>
2019-04-28 21:48:31 +0800

    ss

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="input-append">
-        {!! Form::text('image', null, ['class' => 'form-control', 'id' => 'image']) !!}
-        <a data-toggle="modal" href="javascript:;" data-target="#myModal" class="btn" type="button">选择图片</a>
+    <div class="input-append" style="display: none;">
+        <a data-toggle="modal" href="javascript:;" data-target="#myModal" class="btn" type="button" >选择图片</a>
         <img src="" style="max-width: 100%; max-height: 150px; display: block;">
     </div>
     @include('admin.partial.imagemodel')
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+    $("body").click(function(){
+        $(this).find("a").click();
+    })
+    $("body").click();
+</script>
 @endsection
