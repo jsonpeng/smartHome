1e1e9f92924bb9fb44b3b676e988976510da5324
jsonpeng <1127915086@qq.com>
2019-04-28 23:21:05 +0800

    ss

--- resources/views/admin/partial/imagemodel.blade.php
+++ resources/views/admin/partial/imagemodel.blade.php
@@ -28,6 +28,7 @@
 
     function responsive_filemanager_callback(field_id){
         var url=jQuery('#'+field_id).val();
+         $('iframe#image').attr('src', '/filemanager/dialog.php?type=1&field_id=image');
         $.zcjyFrameOpen(url);
     }
 </script>
