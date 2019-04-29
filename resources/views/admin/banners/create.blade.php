348f19e2a8077c34ec998061ee425d8a06ec0582
jsonpeng <1127915086@qq.com>
2019-04-29 16:57:44 +0800

    ss

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
     <div class="input-append" style="text-align: center;">
-        <a data-toggle="modal" href="javascript:;" data-target="#myModal" class="btn" type="button" >查看设计图片</a>
+        <a data-toggle="modal" href="javascript:;" data-target="#myModal" class="btn" type="button" id="seeImg">查看设计图片</a>
         <img src="" style="width: 100%; height: auto; display: block;">
     </div>
     @include('admin.partial.imagemodel')
 @endsection
 
 @section('scripts')
-
+<script type="text/javascript">
+	$("#seeImg").click();
+</script>
 @endsection
