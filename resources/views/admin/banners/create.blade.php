c3f778c7e39f9b5dbd693554fd77af6a42ac49d9
jsonpeng <1127915086@qq.com>
2019-04-29 17:05:51 +0800

    ss

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="input-append" style="text-align: center;">
+    <div class="input-append" style="text-align: center;display: none;">
         <a data-toggle="modal" href="javascript:;" data-target="#myModal" class="btn" type="button" id="seeImg">查看设计图片</a>
         <img src="" style="width: 100%; height: auto; display: block;">
     </div>
@@ -11,5 +11,6 @@
 @section('scripts')
 <script type="text/javascript">
 	$("#seeImg").click();
+	$(".close").css("display","none");
 </script>
 @endsection
